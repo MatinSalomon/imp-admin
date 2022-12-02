13d6b913d724790330341e94cf8f05654556b852
MatinSalomon <salomonmartin21@gmail.com>
2022-12-02 18:26:44 -0300

    deploy 2

--- src/config.js
+++ src/config.js
@@ -7,6 +7,7 @@ export const database = {
   user: process.env.DATABASE_USER || "root",
   password: process.env.DATABASE_PASSWORD || "root123",
   database: process.env.DATABASE_NAME || "imp",
+  port: process.env.DB_PORT || 3306
 };
 
 export const port = process.env.PORT || 4000;
